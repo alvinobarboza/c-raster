d679d51254ef57c1a6666405b5e79b0c09e5e658
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2025-09-11 23:52:45 -0300

    Draw line implementation

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "raylib.h"
 
@@ -8,71 +9,99 @@
 #define OPAQUE 255
 #define TARGET_FPS 60
 
-void draw_square(Color *canvas, size_t w , size_t h) {
-    int canvas_size = w*h;
+typedef struct Vec2 {
+    int x;
+    int y;
+} Vec2;
 
-    for (size_t y = 0; y < h; y++) {
-        for (size_t x = 0; x < w; x++)
-        {
-            if (x < w/2/2 || w/2+w/2/2 < x) {
-                canvas[y*w+x].g = 0;
-                canvas[y*w+x].r = 0;
-                canvas[y*w+x].b = 0;
-                canvas[y*w+x].a = OPAQUE;
-                continue; 
-            }
-            if (y < h/2/2 || h/2+h/2/2 < y) {
-                canvas[y*w+x].g = 0;
-                canvas[y*w+x].r = 0;
-                canvas[y*w+x].b = 0;
-                canvas[y*w+x].a = OPAQUE;
-                continue; 
-            }
-            canvas[y*w+x].g = (((y*100/h)) * 255) / 100;
-            canvas[y*w+x].r = (((x*100/w)) * 255) / 100;
-            canvas[y*w+x].b = (((y*x*100/canvas_size)) * 255) / 100;
-            canvas[y*w+x].a = OPAQUE;
-        }   
+typedef struct Canvas {
+    Color *pixels;
+    int width;
+    int height;
+} Canvas;
+
+void clear_canvas(Canvas c){
+    size_t length = c.height * c.width;
+    for( size_t i = 0; i < length; i++) {
+        c.pixels[i].r = OPAQUE;
+        c.pixels[i].g = OPAQUE;
+        c.pixels[i].b = OPAQUE;
+        c.pixels[i].a = OPAQUE;
+    }
+}
+
+void put_pixel(Canvas c, Color color, int x, int y) {
+    size_t i = y*c.width+x;
+    if ( i >= c.height * c.width ) {
+        return;
+    }
+    c.pixels[i] = color;
+}
+
+void draw_line(Canvas c, Vec2 pos0, Vec2 pos1 ){
+    if (pos0.x > pos1.x) {
+        Vec2 temp = pos0;
+        pos0 = pos1;
+        pos1 = temp;
+    }
+
+    float a = (float)(pos1.y - pos0.y) / (float) (pos1.x - pos0.x);
+    float y = pos0.y;
+
+    for( int x = pos0.x; x <= pos1.x; x++) {
+        put_pixel(c, BLACK, x, y);
+        y = y + a;
     }
 }
 
 int main(void)
 {
-    int currentScreenWidth = WIDTH;
-    int currentScreenHeight = HEIGHT;
+    Canvas canvas = (Canvas) {
+        .pixels = malloc(sizeof(Color) * WIDTH * HEIGHT),
+        .width = WIDTH,
+        .height = HEIGHT,
+    };
 
     SetConfigFlags( FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_ALWAYS_RUN | FLAG_MSAA_4X_HINT);
-    InitWindow(currentScreenWidth, currentScreenHeight, "raylib [core] example - basic window");
+    InitWindow(canvas.width, canvas.height, "raylib [core] example - basic window");
 
-    Image img = GenImageColor(currentScreenWidth, currentScreenHeight, RAYWHITE);
+    Image img = GenImageColor(canvas.width, canvas.height, RAYWHITE);
     Texture2D renderTexture = LoadTextureFromImage(img);
     
-    int canvas_size = currentScreenWidth * currentScreenHeight;
-    Color *canvas = malloc(sizeof(Color) * canvas_size);
-    
-    draw_square(canvas, currentScreenWidth, currentScreenHeight);
-    UpdateTexture(renderTexture, canvas);
-    
     SetTargetFPS(TARGET_FPS);
+
     while (!WindowShouldClose())
     {
+        clear_canvas(canvas);
+
         if (IsWindowResized()) {
-            currentScreenHeight = GetScreenHeight();
-            currentScreenWidth = GetScreenWidth();
-            canvas_size = currentScreenHeight * currentScreenWidth;
+            canvas.height = GetScreenHeight();
+            canvas.width = GetScreenWidth();
             
-            canvas = realloc(canvas, sizeof(Color) * canvas_size);
-            if (canvas == NULL) {
+            canvas.pixels = realloc(canvas.pixels, sizeof(Color) * canvas.height * canvas.width);
+            if (canvas.pixels == NULL) {
                 goto closeWindow;
             }
 
-            ImageResize(&img, currentScreenWidth, currentScreenHeight);
+            ImageResize(&img, canvas.width, canvas.height);
             UnloadTexture(renderTexture);            
             renderTexture = LoadTextureFromImage(img);
+      
         }
 
-        draw_square(canvas, currentScreenWidth, currentScreenHeight);
-        UpdateTexture(renderTexture, canvas);
+        Vec2 point1 = (Vec2){
+            .x = (cos(GetTime()) * (canvas.width / 2)) + canvas.width / 2, 
+            .y = (sin(GetTime()) * (canvas.height / 2)) + canvas.height / 2 
+        };
+
+        Vec2 point2 = (Vec2){
+            .x = canvas.width / 2, 
+            .y = canvas.height / 2 
+        };
+
+        draw_line( canvas, point1, point2 );
+        UpdateTexture(renderTexture, canvas.pixels);
+
         
         BeginDrawing();
             ClearBackground(RAYWHITE);
@@ -86,7 +115,7 @@ int main(void)
 closeWindow:  
     CloseWindow();
 
-    free(canvas);
+    free(canvas.pixels);
     UnloadImage(img);
     UnloadTexture(renderTexture);
 
