025e5335c9e5df08fe7c8d93b79dd81b0e929f71
Alvino Barboza <alvinobarboza@gmail.com>
2025-09-16 14:49:55 -0300

    Aspect ratio calculations

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #include "raylib.h"
-#include "vector.h"
+#include "transforms.h"
 #include "camera.h"
 #include "model3d.h"
 #include "drawing.h"
@@ -43,17 +43,17 @@ Triangle tris[] = {
 
 int main(void)
 {
-    Cam canvas = (Cam) {
+    Cam camera = (Cam) {
         .canvas = malloc(sizeof(Color) * WIDTH * HEIGHT),
         .width = WIDTH,
         .height = HEIGHT,
-        .view = (Viewport) {.d = 1.0f, .height = 1.0f, .width = 1.0f}
+        .view = (Viewport) {.d = 1.0f, .height = 1.0f, .width = (float)WIDTH / (float)HEIGHT}
     };
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_ALWAYS_RUN | FLAG_MSAA_4X_HINT);
-    InitWindow(canvas.width, canvas.height, "raylib [core] example - basic window");
+    InitWindow(camera.width, camera.height, "raylib [core] example - basic window");
 
-    Image img = GenImageColor(canvas.width, canvas.height, RAYWHITE);
+    Image img = GenImageColor(camera.width, camera.height, RAYWHITE);
     Texture2D renderTexture = LoadTextureFromImage(img);
     
     SetTargetFPS(TARGET_FPS);
@@ -65,35 +65,47 @@ int main(void)
         .vertsCount = 8,
     };
 
-    Instance instance = (Instance) {
-        .model = &cube,
-        .rotation = (Vec3){0},
-        .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
-        .transform = (Vec3){.x = -1.5, .y = 0.0f, .z = 7.0f},
+    Instance instances[2] = {
+        (Instance) {
+            .model = &cube,
+            .rotation = (Vec3){0},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
+            .translation = (Vec3){.x = -1.5, .y = 0.0f, .z = 7.0f},
+        },
+        (Instance) {
+            .model = &cube,
+            .rotation = (Vec3){0},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
+            .translation = (Vec3){.x = 1.25, .y = 2.0f, .z = 7.5f},
+        }
     };
 
     while (!WindowShouldClose())
     {
-        clear_canvas(canvas);
+        clear_canvas(camera);
 
         if (IsWindowResized()) {
-            canvas.height = GetScreenHeight();
-            canvas.width = GetScreenWidth();
+            camera.height = GetScreenHeight();
+            camera.width = GetScreenWidth();
             
-            canvas.canvas = realloc(canvas.canvas, sizeof(Color) * canvas.height * canvas.width);
-            if (canvas.canvas == NULL) {
+            camera.canvas = realloc(camera.canvas, sizeof(Color) * camera.height * camera.width);
+            if (camera.canvas == NULL) {
                 goto closeWindow;
             }
 
-            ImageResize(&img, canvas.width, canvas.height);
+            camera.view.width = (float)camera.width / (float)camera.height;
+
+            ImageResize(&img, camera.width, camera.height);
             UnloadTexture(renderTexture);            
             renderTexture = LoadTextureFromImage(img);
       
         }
 
-        render_model(canvas, instance);
+        for(size_t i = 0; i < 2; i++){
+            render_model(camera, instances[i]);
+        }
 
-        UpdateTexture(renderTexture, canvas.canvas);
+        UpdateTexture(renderTexture, camera.canvas);
 
         
         BeginDrawing();
@@ -107,7 +119,7 @@ int main(void)
     }
 
 closeWindow:      
-    free(canvas.canvas);
+    free(camera.canvas);
     UnloadImage(img);
     UnloadTexture(renderTexture);
 
