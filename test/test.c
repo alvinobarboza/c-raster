67633fd560a23f4325e1acf46399747d0de21b58
Alvino Barboza <alvinobarboza@gmail.com>
2025-10-06 11:43:04 -0300

    Testing

--- test/test.c
+++ test/test.c
@@ -4,7 +4,14 @@ int main() {
 	// ModelData ammoBox = load_model_from_path("./assets/ammo_box_1_1.obj", "./assets/ammo_mp_1.png", true);
 	// free_model(&ammoBox);
 	
-    ModelData teapot = load_model_from_path("./assets/newell_teaset/teapot.obj", NULL, false);
-	free_model(&teapot);
+    // ModelData teapot = load_model_from_path("./assets/newell_teaset/teapot.obj", NULL, true);
+    // ModelData teacup = load_model_from_path("./assets/newell_teaset/teacup.obj", NULL, true);
+    ModelData spoon = load_model_from_path("./assets/newell_teaset/spoon.obj", NULL, true);
+    ModelData cube = load_model_from_path("./assets/newell_teaset/cube.obj", NULL, true);
+
+    // free_model(&teapot);
+    // free_model(&teacup);
+    free_model(&spoon);
+    free_model(&cube);
 	return 0;
 }
