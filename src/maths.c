e59b959a3c4d6316bf228cf06a5c6175198ef1bf
Alvino Barboza <alvinobarboza@gmail.com>
2025-10-15 17:58:03 -0300

    fix - lerp function was backwards

--- src/maths.c
+++ src/maths.c
@@ -1,7 +1,7 @@
 #include "maths.h"
 
-float lerp_a_b(float a, float b, float ratio) {
-	if (ratio > 1) return b;
-	if (ratio < 0) return a;
-	return a * ratio + b * (1 - ratio);
+float lerp_a_b(float a, float b, float t) {
+	if (t > 1) return b;
+	if (t < 0) return a;
+	return a + t * (b - a);
 }
