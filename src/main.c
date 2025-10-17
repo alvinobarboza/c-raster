886dea7b505e60a4f7d2748dbcccc8a4b7b9514b
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2025-10-16 23:11:39 -0300

    Showcase scene - TODO - Correct normal rotation

--- src/main.c
+++ src/main.c
@@ -66,25 +66,49 @@ int main(void)
 
     Cam camera = init_camera(
         WIDTH, HEIGHT, 
-        (Vec3) {.x = 0.0f, .y = 0.0f, .z = 0.0f},
-        (Vec3) {.x = 0.0f, .y = 0.0f, .z = 0.0f}
-        // (Vec3) {.x = 1.69f, .y = 0.38f, .z = 3.06f},
-        // (Vec3) {.x = 11.27f, .y = 92.73f, .z = 0}
+        // (Vec3) {.x = 0.0f, .y = 0.0f, .z = 0.0f},
+        // (Vec3) {.x = 0.0f, .y = 0.0f, .z = 0.0f}
+        (Vec3) {.x = .8f, .y = 1.88f, .z = .05f},
+        (Vec3) {.x = -44.74f, .y = 35.64f, .z = 0}
     );
 
-    ModelData teapot = load_model_from_path("./assets/newell_teaset/teapot.obj", NULL, false, false, true);
-    ModelData teacup = load_model_from_path("./assets/newell_teaset/teacup.obj", NULL, false, false, true);
-    ModelData spoon = load_model_from_path("./assets/newell_teaset/spoon.obj", NULL, false, false, true);
-    ModelData keyboard = load_model_from_path(
-        "./assets/psx_pizza_doggy/pc_keyboard_mp_3.obj", "./assets/psx_pizza_doggy/pc_accessories_mp_1.png", false, false, true);
+    ModelData teapot = load_model_from_path("./assets/newell_teaset/teapot.obj", "./assets/psx_pizza_doggy/metal_mp_3.png", false, false, true);
+    ModelData teacup = load_model_from_path("./assets/newell_teaset/teacup.obj", "./assets/psx_pizza_doggy/plastic_3.png", false, false, true);
+    ModelData spoon = load_model_from_path("./assets/newell_teaset/spoon.obj", "./assets/psx_pizza_doggy/metal_mp_3.png", false, false, true);
+    
+    ModelData floor = load_model_from_path(
+        "./assets/psx_pizza_doggy/floor_ceiling_1.obj", "./assets/psx_pizza_doggy/floor_1.png", false, false, true);
+    
     ModelData ammo = load_model_from_path(
         "./assets/psx_pizza_doggy/ammo_box_1_1.obj", "./assets/psx_pizza_doggy/ammo_mp_1.png", false, false, true);
-    ModelData monitor = load_model_from_path(
-        "./assets/psx_pizza_doggy/pc_monitor_mp_1.obj", "./assets/psx_pizza_doggy/pc_monitor_mp_1.png", false, false, true);
+    
+    ModelData ammoPistol = load_model_from_path(
+        "./assets/psx_pizza_doggy/ammo_pistol_loose_5pcs_mp_1.obj", "./assets/psx_pizza_doggy/ammo_mp_1.png", false, false, true);
+    
+    ModelData chair = load_model_from_path(
+        "./assets/psx_pizza_doggy/chair_mp_3.obj", "./assets/psx_pizza_doggy/wood_3.png", false, false, true);
+    
     ModelData flashlight = load_model_from_path(
         "./assets/psx_pizza_doggy/flashlight_1.obj", "./assets/psx_pizza_doggy/flashlight_1.png", false, false, true);
+    
+    ModelData matchbox = load_model_from_path(
+        "./assets/psx_pizza_doggy/matchbox_2.obj", "./assets/psx_pizza_doggy/matchbox_1.png", false, false, true);
+    
+    ModelData keyboard = load_model_from_path(
+        "./assets/psx_pizza_doggy/pc_keyboard_mp_3.obj", "./assets/psx_pizza_doggy/pc_accessories_mp_1.png", false, false, true);
+    
+    ModelData monitor = load_model_from_path(
+        "./assets/psx_pizza_doggy/pc_monitor_mp_1.obj", "./assets/psx_pizza_doggy/pc_monitor_mp_1.png", false, false, true);
+    
+    ModelData pistolMag = load_model_from_path(
+        "./assets/psx_pizza_doggy/pistol_mp_1_mag_empty.obj", "./assets/psx_pizza_doggy/pistols_mp_1.png", false, false, true);
+    
+    ModelData table = load_model_from_path(
+        "./assets/psx_pizza_doggy/table_large_2.obj", "./assets/psx_pizza_doggy/wood_3.png", false, false, true);
+    
     ModelData vhs = load_model_from_path(
         "./assets/psx_pizza_doggy/vhs_tape_1.obj", "./assets/psx_pizza_doggy/vhs_tape_1.png", false, false, true);
+    
     ModelData marbleBust = load_model_from_path(
         "./assets/polyhaven_rico_b3d/marble_bust.obj", "./assets/polyhaven_rico_b3d/marble_bust.jpg", false, false, true);
    
@@ -92,58 +116,83 @@ int main(void)
     // ModelData fakeGodRays = load_model_from_path(
     //     "./assets/psx_pizza_doggy/fake_light_window_2_double.obj", "./assets/psx_pizza_doggy/fake_light_mp_1.png", false, false, true);
 
-    // ModelData icosahedron = icosahedron_shape();
-    // ModelData torus = torus_shape();
-    // ModelData uvSphere = uv_sphere_shape();
-    // ModelData triangle = triangle_shape();
-
     // TODO: better instance loading
     Instance instances[] = {
-        init_instance(&marbleBust, (Transforms){
-            .position = (Vec3){.x = 0.0f, .y = 0.0f, .z = 2.0f},
-            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
-            .scale = (Vec3){.x = 1.5f, .y = 1.5f, .z = 1.5f}
-        }),
-        init_instance(&vhs, (Transforms){
-            .position = (Vec3){.x = 2.0f, .y = 0.0f, .z = 1.0f},
+        init_instance(&floor, (Transforms){
+            .position = (Vec3){.x = 0.0f, .y = 0.0f, .z = 1.0f},
             .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
             .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
-        }),
-        init_instance(&flashlight, (Transforms){
-            .position = (Vec3){.x = 1.6f, .y = 0.0f, .z = 1.0f},
-            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
-            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
-        }),
-        init_instance(&ammo, (Transforms){
-            .position = (Vec3){.x = 1.4f, .y = 0.0f, .z = 1.0f},
-            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
-            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
-        }),
-        init_instance(&keyboard, (Transforms) {
-            .position = (Vec3){.x = 1.0f, .y = 0.0f, .z = 1.0f},
-            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
-            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
-        }),
-        init_instance(&monitor, (Transforms) {
-            .position = (Vec3){.x = 0.4f, .y = 0.0f, .z = 1.0f},
-            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
-            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
         }),
         init_instance(&spoon, (Transforms){
-            .position = (Vec3){.x = 0.2f, .y = 0.0f, .z = 1.0f},
-            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
+            .position = (Vec3){.x = 0.2f, .y = .97f, .z = 1.0f},
+            .rotation = (Vec3){.x = 10.0f, .y = 0.0f, .z = 0.0f},
             .scale = (Vec3){.x = 1.0f/8.0f, .y = 1.0f/8.0f, .z = 1.0f/8.0f}
         }),
         init_instance(&teacup, (Transforms){
-            .position = (Vec3){.x = 0.0f, .y = 0.0f, .z = 1.0f},
+            .position = (Vec3){.x = -0.4f, .y = .97f, .z = 0.9f},
             .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
             .scale = (Vec3){.x = 1.0f/8.0f, .y = 1.0f/8.0f, .z = 1.0f/8.0f}
         }),
         init_instance(&teapot, (Transforms) {
-            .position = (Vec3){.x = -.5, .y = 0.0f, .z = 1.0f},
+            .position = (Vec3){.x = -1.0, .y = 0.0f, .z = 1.0f},
             .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
             .scale = (Vec3){.x = 1.0f/9.0f, .y = 1.0f/8.0f, .z = 1.0f/8.0f},
-        })
+        }),
+        init_instance(&ammo, (Transforms){
+            .position = (Vec3){.x = -.07f, .y = .97f, .z = 1.0f},
+            .rotation = (Vec3){.x = 0.0f, .y = -45.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&ammoPistol, (Transforms){
+            .position = (Vec3){.x = .1f, .y = .97f, .z = 1.0f},
+            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&chair, (Transforms){
+            .position = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.5f},
+            .rotation = (Vec3){.x = 0.0f, .y = -200.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&flashlight, (Transforms){
+            .position = (Vec3){.x = -.6f, .y = .97f, .z = 1.3f},
+            .rotation = (Vec3){.x = 0.0f, .y = -160.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&matchbox, (Transforms){
+            .position = (Vec3){.x = -.6f, .y = .97f, .z = .64f},
+            .rotation = (Vec3){.x = 0.0f, .y = 40.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&keyboard, (Transforms) {
+            .position = (Vec3){.x = 0.5f, .y = .97f, .z = .9f},
+            .rotation = (Vec3){.x = 0.0f, .y = -20.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
+        }),
+        init_instance(&monitor, (Transforms) {
+            .position = (Vec3){.x = 0.6f, .y = .97f, .z = 1.2f},
+            .rotation = (Vec3){.x = 0.0f, .y = -30.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f},
+        }),
+        init_instance(&pistolMag, (Transforms){
+            .position = (Vec3){.x = .24f, .y = .99f, .z = .7f},
+            .rotation = (Vec3){.x = 90.0f, .y = 90.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&table, (Transforms){
+            .position = (Vec3){.x = 0.0f, .y = 0.0f, .z = 1.0f},
+            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&vhs, (Transforms){
+            .position = (Vec3){.x = -.05f, .y = .97f, .z = 1.24f},
+            .rotation = (Vec3){.x = 0.0f, .y = -62.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
+        init_instance(&marbleBust, (Transforms){
+            .position = (Vec3){.x = -1.3, .y = 0.0f, .z = .6f},
+            .rotation = (Vec3){.x = 0.0f, .y = 0.0f, .z = 0.0f},
+            .scale = (Vec3){.x = 1.0f, .y = 1.0f, .z = 1.0f}
+        }),
         // init_instance(&fakeGodRays, (Transforms) {
         //     .position = (Vec3){.x = 0.25, .y = 1.5f, .z = 10.0f},
         //     .rotation = (Vec3){.x = 0.0f, .y = 95.0f, .z = 0.0f},
